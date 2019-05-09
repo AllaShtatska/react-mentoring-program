bc4ce1a1f20459e1eb8ebdfccbe69fda9673159e
Alla Shtatska <ashtatska@attune.gshapps.net>
2019-05-09 19:55:44 +0300

    Prepare for SSR - step 0

---
+++ src/serverRenderer.js
@@ -0,0 +1,20 @@
+//middleware for express to render html
+function renderHTML() {
+  return `
+    <!doctype html>
+    <html>
+        <head>
+            <meta charset=utf-8>
+            <title>React Server Side Rendering</title>
+        </head>
+        <body>
+            <div id="root"></div>
+            <script src="/js/main.js"></script>
+        </body>
+    </html>
+    `;
+}
+
+module.exports = (req, res) => {
+  res.send(renderHTML());
+};
